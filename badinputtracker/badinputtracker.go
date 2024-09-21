c3ae89f776f0afe4cba97aa506035a453f9c0a36
Volte6 <143822+Volte6@users.noreply.github.com>
2024-09-21 15:11:52 -0700

    Adding badinput tracker (#80)
    # Changes
    * Rejected input from users is tracked in-memory with a counter
    * Added an admin-specific command to list them: `badcommands`

---
+++ badinputtracker/badinputtracker.go
@@ -0,0 +1,36 @@
+package badinputtracker
+
+import "sync"
+
+var (
+	lock        = sync.Mutex{}
+	badCommands = map[string]map[string]int{}
+)
+
+func TrackBadCommand(cmd string, rest string) {
+
+	lock.Lock()
+	defer lock.Unlock()
+
+	if _, ok := badCommands[cmd]; !ok {
+		badCommands[cmd] = map[string]int{}
+	}
+
+	badCommands[cmd][rest] = badCommands[cmd][rest] + 1
+}
+
+func GetBadCommands() map[string]int {
+
+	lock.Lock()
+	defer lock.Unlock()
+
+	ret := map[string]int{}
+
+	for cmd, other := range badCommands {
+		for rest, ct := range other {
+			ret[cmd+` `+rest] = ct
+		}
+	}
+
+	return ret
+}
