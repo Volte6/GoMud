019bddca58211d38b2341ac601d798c12cc0ab19
Volte6 <143822+Volte6@users.noreply.github.com>
2024-10-25 20:42:11 -0700

    adding put command to mobcommands

---
+++ mobcommands/put.go
@@ -0,0 +1,106 @@
+package mobcommands
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/volte6/gomud/items"
+	"github.com/volte6/gomud/mobs"
+	"github.com/volte6/gomud/rooms"
+	"github.com/volte6/gomud/util"
+)
+
+func Put(rest string, mob *mobs.Mob, room *rooms.Room) (bool, error) {
+
+	args := util.SplitButRespectQuotes(strings.ToLower(rest))
+
+	if len(args) < 2 {
+		return true, nil
+	}
+
+	containerName := ``
+	nameSearch := ``
+	for i := len(args) - 1; i >= 1; i-- {
+		if len(nameSearch) > 0 {
+			nameSearch = ` ` + nameSearch
+		}
+		nameSearch = args[i] + nameSearch
+
+		containerName = room.FindContainerByName(nameSearch)
+		if containerName != `` {
+			args = args[:i]
+			break
+		}
+	}
+
+	if containerName == `` {
+		return true, nil
+	}
+
+	container := room.Containers[containerName]
+
+	if len(args) < 1 {
+		return true, nil
+	}
+
+	var item items.Item
+	var itemFound bool
+	goldAmt := 0
+
+	if len(args) >= 2 && args[1] == `gold` {
+
+		g, _ := strconv.ParseInt(args[0], 10, 32)
+		goldAmt = int(g)
+		if goldAmt < 0 {
+			goldAmt = -1 * goldAmt
+		}
+
+	} else {
+
+		item, itemFound = mob.Character.FindInBackpack(strings.Join(args, ` `))
+		if !itemFound && len(args) > 1 {
+			item, itemFound = mob.Character.FindInBackpack(args[0])
+		}
+	}
+
+	if !itemFound && goldAmt == 0 {
+		return true, nil
+	}
+
+	if goldAmt > 0 {
+		container.Gold += goldAmt
+		room.SendText(fmt.Sprintf(`<ansi fg="mobname">%s</ansi> places some <ansi fg="gold">gold</ansi> into the <ansi fg="container">%s</ansi>`, mob.Character.Name, containerName))
+	}
+
+	if itemFound {
+		container.AddItem(item)
+		mob.Character.RemoveItem(item)
+
+		room.SendText(fmt.Sprintf(`<ansi fg="mobname">%s</ansi> places their <ansi fg="itemname">%s</ansi> into the <ansi fg="container">%s</ansi>`, mob.Character.Name, item.DisplayName(), containerName))
+
+		// current hard limit of 10 max items.
+		if len(container.Items) > 10 {
+
+			randItemToRemove := util.Rand(len(container.Items))
+			oopsItem := container.Items[randItemToRemove]
+
+			// get all items that spawn in chests
+			for _, spn := range room.SpawnInfo {
+				if spn.Container == containerName && oopsItem.ItemId == spn.ItemId {
+					// Don't let this one pop out
+					oopsItem = item
+					break
+				}
+			}
+
+			container.RemoveItem(oopsItem)
+			room.SendText(fmt.Sprintf(`The <ansi fg="container">%s</ansi> is too full and a <ansi fg="itemname">%s</ansi> falls out and onto the floor.`, containerName, oopsItem.DisplayName()))
+			room.AddItem(oopsItem, false)
+		}
+	}
+
+	room.Containers[containerName] = container
+
+	return true, nil
+}
