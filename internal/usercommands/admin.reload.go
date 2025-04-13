253e2a96c0f3b4cee0590d52d7e196f7e3cc895e
lostsnow <lostsnow@gmail.com>
2025-04-13 22:43:39 +0800

    Add cmd reload translations

--- internal/usercommands/admin.reload.go
+++ internal/usercommands/admin.reload.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/GoMudEngine/GoMud/internal/events"
 	"github.com/GoMudEngine/GoMud/internal/items"
+	"github.com/GoMudEngine/GoMud/internal/language"
 	"github.com/GoMudEngine/GoMud/internal/rooms"
 	"github.com/GoMudEngine/GoMud/internal/templates"
 	"github.com/GoMudEngine/GoMud/internal/users"
@@ -26,6 +27,13 @@ func Reload(rest string, user *users.UserRecord, room *rooms.Room, flags events.
 	case `items`:
 		items.LoadDataFiles()
 		user.SendText(`Items reloaded.`)
+	case `translations`:
+		ok := language.ReloadTranslation()
+		if !ok {
+			user.SendText(`Translations reload failed.`)
+		} else {
+			user.SendText(`Translations reloaded.`)
+		}
 	default:
 		user.SendText(`Unknown reload command.`)
 	}
